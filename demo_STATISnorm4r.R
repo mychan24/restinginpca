61816b9ae58a0e5701f346fe1b16599d22b359b2
Ju-Chi <Ju-Chi.Yu@utdallas.edu>
2018-12-05 13:10:13 -0600

    Commit from RStudio-JCY

---
+++ demo_STATISnorm4r.R
@@ -0,0 +1,19 @@
+#================================================================#
+### Demonstrating STATIS normalization on a correlation matrix ###
+#================================================================#
+## load packages ----
+
+## create examplar data ---- 
+### create a symmetric matrix with values resembles correlation
+### fake data: 
+####a rectangular matrix (need also to plan for blocks of high correlation)
+
+### fake correlation: 
+
+#### check for blocks on diagonal
+
+### double centering:
+
+### get the first eigenvalue:
+
+###
