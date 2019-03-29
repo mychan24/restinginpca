b3973e488671b625bdb6c75126ce607b52656556
Micaela Chan <4413270+mychan24@users.noreply.github.com>
2019-03-29 15:18:51 -0500

    script to make grandtable MSC01 + MSC02

---
+++ mic/gt_msc01_msc02.R
@@ -0,0 +1,19 @@
+# MSC01 and MSC02
+
+rm(list=ls())
+
+source("./tools/cube2gt.R")
+source("./tools/label_edges.R")
+
+cubepath <- list("./data/zmat/sub-MSC01_zcube_rcube.Rdata",
+                 "./data/zmat/sub-MSC02_zcube_rcube.Rdata")
+
+commpath <- list("./data/parcel_community/sub-MSC01_node_parcel_comm.txt",
+                 "./data/parcel_community/sub-MSC02_node_parcel_comm.txt")
+
+
+p <- cube2gt(cube_paths = cubepath, comm_paths = commpath, 
+             out_file = sprintf("./data/grandatble_and_labels_MSC01_MSC02_%s.Rdata",format(Sys.time(),"%Y%m%d")))
+
+# Sanity check
+# superheat::superheat(X = p[[1]], membership.cols = p[[2]]$wb)
