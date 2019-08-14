d54b802c509702eda99796e3dd2058c6ca8f0802
Micaela Chan <4413270+mychan24@users.noreply.github.com>
2019-08-13 19:03:20 -0500

    Switch define paths and subs definition from hard-coded to sapply() in gt_allsubs.R

--- tools/gt_allsubs.R
+++ tools/gt_allsubs.R
@@ -1,4 +1,5 @@
 # Make Grand Table with all 10 MSC subjects - with double centered correlation matrix
+# 8/13/2019 - Use sapply for paths and subs
 # 8/1/2019
 
 rm(list=ls())
@@ -7,50 +8,23 @@ source("./tools/cube2gt.R")
 source("./tools/label_edges.R")
 library(tidyr)
 
-cube_paths <- list("./data/zmat/sub-MSC01_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC02_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC03_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC04_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC05_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC06_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC07_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC08_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC09_zcube_rcube.Rdata",
-                   "./data/zmat/sub-MSC10_zcube_rcube.Rdata")
+#### ==== EDIT THIS PART ==== ####
+# switch to FALSE if not double centered
+double_center <- TRUE 
+#### ======================== ####
 
-comm_paths <- list("./data/parcel_community/sub-MSC01_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC02_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC03_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC04_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC05_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC06_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC07_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC08_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC09_node_parcel_comm.txt",
-                   "./data/parcel_community/sub-MSC10_node_parcel_comm.txt"
-)
+# Define paths where cubes, community are saved and subject naem
+cube_paths <- sapply(X = 1:10, FUN=function(x){sprintf("./data/zmat/sub-MSC%02d_zcube_rcube.Rdata",x)})
+comm_paths <- sapply(X = 1:10, FUN=function(x){sprintf("./data/parcel_community/sub-MSC%02d_node_parcel_comm.txt",x)})
+subj_list <- paste("sub", sprintf("%02d",1:10), sep="")
 
-subj_list <- c("sub01","sub02", "sub03",
-               "sub04","sub05", "sub06",
-               "sub07","sub08", "sub09","sub10")
-
-p <- cube2gt(cube_paths = cube_paths, comm_paths = comm_paths, 
-             out_file = sprintf("./data/grandatble_and_labels_MSC_allsubs_DC_%s.Rdata",format(Sys.time(),"%Y%m%d")),
-             subj_list=subj_list, double_cent = TRUE)
-
-## With only subject 1 ----
-# cube_paths <- list("./data/zmat/sub-MSC01_zcube_rcube.Rdata")
-# 
-# comm_paths <- list("./data/parcel_community/sub-MSC01_node_parcel_comm.txt"                  )
-# 
-# subj_list <- c("sub01")
-# 
-# p <- cube2gt(cube_paths = cube_paths, comm_paths = comm_paths, 
-#              out_file = sprintf("./data/grandatble_and_labels_MSC_subs1_%s.Rdata",format(Sys.time(),"%Y%m%d")),
-#              subj_list=subj_list)
-
-## Sanity check ----
-# superheat::superheat(X = p[[1]],
-#                      membership.cols = p[[2]]$subjects_edge_label,
-#                      y.axis.reverse = T,
-#                      grid.vline = F)
+## Run and save double-centered or non-double-ceneterd gt table with diff out_file and doubt_cent option
+if(double_center==TRUE){
+  p <- cube2gt(cube_paths = cube_paths, comm_paths = comm_paths,
+               out_file = sprintf("./data/grandatble_and_labels_MSC_allsubs_DC_%s.Rdata",format(Sys.time(),"%Y%m%d")),
+               subj_list=subj_list, double_cent = TRUE)
+  }else{
+    p <- cube2gt(cube_paths = cube_paths, comm_paths = comm_paths, 
+             out_file = sprintf("./data/grandatble_and_labels_MSC_allsubs_%s.Rdata",format(Sys.time(),"%Y%m%d")),
+             subj_list=subj_list, double_cent = FALSE)
+  }
