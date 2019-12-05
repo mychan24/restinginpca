24fecaa635fc1be69144b125bebb48f097251c21
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2019-12-05 16:51:39 -0600

    Add Figure 2

--- presentation/OHBM2020/MakeFigure.R
+++ presentation/OHBM2020/MakeFigure.R
@@ -5,30 +5,33 @@
 #===================================================
 rm(list = ls())
 # Read data ----
-load("BigNet_11.RData")
+load("././MuSu_simattack_4rows_18_HMFA_sub-edgetype.RData")
 library(RColorBrewer)
 library(pals)
 library(PTCA4CATA)
 library(dplyr)
 library(plyr)
 
-# Scree plots ----
-PlotScree(svd.res$ExPosition.Data$eigs,title = "",plotKaiser = T,lwd4Kaiser = 1)
-Scree <- recordPlot()
+# Fig 2A
+f.labels <- createxyLabels.gen(x_axis = 1, y_axis = 2,
+                               lambda = svd.res$ExPosition.Data$eigs,
+                               tau = svd.res$ExPosition.Data$t,
+                               axisName = "Component ")
 
-# Plot sessions with pFs
 plot.f_sess4pF <- createFactorMap(svd.res$ExPosition.Data$fi,
                                   constraints = pF.constraints,
-                                  col.points = "maroon4",
-                                  col.labels = "maroon4",
+                                  col.points = c("maroon4","darkslateblue","maroon4","darkslateblue"),
+                                  col.labels = c("maroon4","darkslateblue","maroon4","darkslateblue"),
+                                  text.cex = 5,
                                   alpha.points = 1,
-                                  col.background = adjustcolor('white',alpha.f = .2))
+                                  col.background = adjustcolor('white',alpha.f = .2),
+                                  title = "Mean Factor Score of Sessions & \nPartial Factor Scores of Subjects")
                                    # with t(gt)
 plot.pf <- createPartialFactorScoresMap(
-  factorScores = svd.res$ExPosition.Data$fi[c(1,2,3,6,7,9),],
-  partialFactorScores = pFi[c(1,2,3,6,7,9),,],#c(1,2,7,8,10)],
+  factorScores = svd.res$ExPosition.Data$fi,
+  partialFactorScores = pFi,#c(1,2,7,8,10)],
   axis1 = 1, axis2 = 2,
-  size.points = 3,
+  size.points = 2,
   size.labels = 2,
   shape.points = 18,
   family.labels = 'sans',
@@ -36,73 +39,94 @@ plot.pf <- createPartialFactorScoresMap(
   font.labels = "bold",
   colors4Blocks = cols25(10)
 )
-f_sess <- plot.f_sess4pF$zeMap + f.labels + plot.pf$mapColByBlocks #(0mid-17right; topofgraybar-7bot)
-f_sess
+f_sess <- plot.f_sess4pF$zeMap_background + f.labels + plot.pf$mapColByBlocks + plot.f_sess4pF$zeMap_dots + plot.f_sess4pF$zeMap_text# + plot.pf$linesColByBlocks + plot.pf$pointsColByBlocks#(0mid-17right; topofgraybar-7bot)
+Fig2A <- f_sess
+Fig2A
 
-# Plot edges
-mean.fj2plot <- mean.fj[which(importantEdg == TRUE),] 
-# mean.fj2plot <- mean.fj
+# Fig 2B
 
-getBoot2plot_edge <- BootCube.Comm$BootCube[importantEdg,c(1,2),]
-# getBoot2plot_edge <- BootCube.Comm$BootCube[,c(1,2),]
+# Loadings
+bar.edge <- mean.edge.fj[importantCommEdg,1]
+names(bar.edge) <- rownames(mean.edge.fj[importantCommEdg,])
+
+edge.show <- names(bar.edge)
+lookup.tab <- CommName[,c(1,4)]
+colnames(lookup.tab) <- c("Num","Short")
+lookup.tab$Short <- gsub('[[:digit:]]+', '', lookup.tab$Short)
+
+name.bar <- c("Within DMN", 
+              "Between DMN & Aud", 
+              "Between DMN & FPN", 
+              "Within hSMN", 
+              "Between hSMN & Aud", 
+              "Within Aud", 
+              "Within lVis", 
+              "Between lVis & hSMN", 
+              "Between lVis & Aud", 
+              "Between lVis & CON", 
+              "Within FPN", 
+              "Between FPN & DAN", 
+              "Between FPN & CON", 
+              "Within DAN", 
+              "Between DAN & hSMN", 
+              "Within CON",
+              "Between CON & hSMN",
+              "Between CON & Aud")
+names(bar.edge) <- name.bar
 
 
-net.edge.col <- as.matrix(mapvalues(net.edge,from = unique(net.edge), to = rep(cols25(10),each = 2)))
-rownames(net.edge.col) <- rownames(c_edge)
-#--- color for networks
-col4ImportantEdg <- net.edge.col # get colors
-col4NS <- 'gray90' # set color for not significant edges to gray
-col4ImportantEdg[!importantEdg] <- col4NS # replace them in the color vector
+Fig2b <- PrettyBarPlot2(bar.edge,horizontal = F,
+               threshold = 0,
+               color.bar = c("darkslateblue","maroon4","gray"),
+               color.bord = c("darkslateblue","maroon4","gray"),
+               color.letter = c("darkslateblue","maroon4","gray"),
+               ylim = c(min(mean.edge.fj[importantCommEdg,1]),
+                        0.01+max(mean.edge.fj[importantCommEdg,1])),
+               main = "\n Mean significant loadings on Component 1")
+
+Fig2b
+
+# Fig 2C
+load("././cfDiSTATIS_simattack_GordonParcel.RData")
+
+f.labels <- createxyLabels.gen(x_axis = x_cp, y_axis = y_cp,
+                               lambda = distatis.res$res4Splus$eigValues,
+                               tau = distatis.res$res4Splus$tau,
+                               axisName = "Component ")
+
+net.fi2plot <- net.fi[c(1:6,8:13),]
+sess.pF.net2plot <- sess.pF.net[c(1:6,8:13),,]
+
+netf.graph4sess <- createFactorMap(net.fi2plot,
+                                   axis1 = x_cp, axis2 = y_cp,
+                                   title = "DiSTATIS - \nMean Factor Score of Networks &\nPartial Factor Scores of Sessions
+",
+                                   col.points = CommColor.gc[rownames(net.fi2plot), "CommColor"],
+                                   col.labels = CommColor.gc[rownames(net.fi2plot), "CommColor"],
+                                   pch = 17,
+                                   alpha.points = 1, cex = 2, text.cex = 3,
+                                   constraints = minmaxHelper4Partial(net.fi,
+                                                                      sess.pF.net))
+
+dimnames(sess.pF.net2plot)[[3]] <- c("1","2","3","4")
+netpf.sess <- createPartialFactorScoresMap(net.fi2plot,
+                                           sess.pF.net2plot,
+                                           axis1 = x_cp, axis2 = y_cp,
+                                           colors4Blocks = c("maroon4","darkslateblue","maroon4","darkslateblue"),
+                                           shape.points = 20,
+                                           size.points = 2, size.labels = 2,
+                                           alpha.points = 1, alpha.lines = .8, alpha.labels = 1,
+                                           names4Partial = dimnames(sess.pF.net2plot)[[3]])
 
 
-fj2plot.sq <- apply(mean.fj2plot[1:9],2,function(x){x^2}) # square the factor scores
-dist2c <- sqrt(fj2plot.sq[,1]+fj2plot.sq[,2]) # take the sqrt of the ss of component 1+2
-fj.ind <- sort(abs(dist2c),decreasing = TRUE, index.return = TRUE)$ix # sort the distance to the origen and get the index (big to small)
-fj2plot.ind <- fj.ind[1:round(length(fj.ind)*.2)] # plot furthest 25% 
-col.max10 <- col4ImportantEdg[which(importantEdg == TRUE)] %>% .[fj2plot.ind]
-# col.max10 <- col4ImportantEdg %>% .[fj2plot.ind]
+f02.netFpF.sess <- netf.graph4sess$zeMap_background + netf.graph4sess$zeMap_text +netpf.sess$mapColByBlocks + netf.graph4sess$zeMap_dots + f.labels
+Fig2c <- f02.netFpF.sess
+Fig2c
 
-plot.f_edge_imp <- createFactorMap(mean.fj2plot[fj2plot.ind,], axis1 = 1, axis2 = 2,
-                                   col.points = col.max10,
-                                   col.labels = col.max10,
-                                   text.cex = 2.5,
-                                   force = 1.5,
-                                   box.padding = 0.05,
-                                   title = "",
-                                   col.background = adjustcolor('white',alpha.f = .2),
-                                   constraints = lapply(minmaxHelper(mean.fj2plot[fj2plot.ind,]),"*",2)
-                                   )
-f_impedge.CI <- MakeCIEllipses(getBoot2plot_edge[fj2plot.ind,,],
-                               names.of.factors = c(sprintf("Factor %s", 1),sprintf("Factor %s", 2)),
-                               col = col.max10,
-                               p.level = .95)
-f_CInetedge <- plot.f_edge_imp$zeMap_background + plot.f_edge_imp$zeMap_dots + plot.f_edge_imp$zeMap_text + f_impedge.CI + f.labels
-f_CInetedge
+library(ggpubr)
 
-# Plot edges type
-plot.f_bw_imp <- createFactorMap(mean.fj.bw, axis1 = 1, axis2 = 2,
-                                 col.points = rep(cols25(10),each = 2),
-                                 col.labels = rep(cols25(10),each = 2),
-                                 # col.points = net.edge.col$gc[rownames(mean.fj.bw),],
-                                 # col.labels = net.edge.col$gc[rownames(mean.fj.bw),],
-                                 text.cex = 3,
-                                 force = 0.5,
-                                 title = "",
-                                 col.background = adjustcolor('white',alpha.f = .2))
-
-plot.f_bw_imp$zeMap + f.labels
-# get the bootstrap data for each type of network edges
-getBoot2plot_edge_bw <- BootCube.Comm.bw$BootCube
-# plot the CI ellipses
-plot.CI.f_bw <- MakeCIEllipses(getBoot2plot_edge_bw[,c(1,2),],
-                               names.of.factors = c(sprintf("Factor %s", 1),sprintf("Factor %s", 2)),
-                               col = rep(cols25(10),each = 2),
-                               # col = as.vector(net.edge.col$gc[rownames(getBoot2plot_edge_bw),]),
-                               p.level = .95)
-# create plot
-# f_CInetedge_bw <- plot.f_bw_imp$zeMap_background + plot.f_bw_imp$zeMap_dots + plot.f_bw_imp$zeMap_text + plot.CI.f_bw + f.labels
-# f_CInetedge_bw
-
-saving.pptx <-  saveGraph2pptx(file2Save.pptx = 'bignet_3', 
-                               title = "" , 
-                               addGraphNames = TRUE)
+png("Figure2.png", width = 400, height = 120, units = 'mm', res = 500)
+ggarrange(Fig2A,Fig2b,Fig2c,
+          labels = c("A","B","C"),
+          ncol = 3, nrow = 1)
+dev.off()
